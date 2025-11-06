8bae993384b32f83d3db1070fe84df1ed670ebc1
Thanas <s6701012610180@email.kmutnb.ac.th>
2025-11-07 03:49:30 +0700

    Clean repo (no large files)

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://nbxuzdrutlvavirpzpjt.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ieHV6ZHJ1dGx2YXZpcnB6cGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzODUzMDYsImV4cCI6MjA3Mzk2MTMwNn0.tD4noFRKDFcA9x-A5udQSjkhKuXbccPdohdFjd8Fyo4"
+);
